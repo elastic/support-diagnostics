622cd81abdf4becc3801ceba073c6862462993a7
Jeremy Falling <projects@falling.se>
2014-10-07 15:46:58 -0700

    Merge branch 'master' of github.com:gmarz/elasticsearch-support-diagnostics into gmarz-master

--- README.md
+++ README.md
@@ -11,21 +11,20 @@ Installation:
 
 ### Support Diagnostics Script
 
-The diagnostic script is installed in ./bin/support-diagnostics.sh.
+The diagnostic script is installed in `./bin/support-diagnostics.sh` (or `./bin/support-diagnostics.bat` on Windows).
 
-In order to gather the elasticsearch config and logs you must run
-support-diagnostics.sh on a node within your elasticsearch cluster.
-If local data, such as top or netstat are required, it should be run
-on each node in the cluster.  Otherwise, running
-support-diagnostics.sh on a single node is sufficient.
+In order to gather the elasticsearch config and logs you must run the script on a node within your elasticsearch cluster.  If local data, such as top or netstat are required, it should be run on each node in the cluster.  Otherwise, running the script on a single node is sufficient.
 
-In most cases no flags are required.
-
-### Usage:
+### Usage
 
     -h  This help message
-    -H  Elasticsearch hostname:port (defaults to localhost:9200)
-    -n  On a host with multiple nodes, specify the node name to gather data for. Value should match node.name as defined in elasticsearch.yml (optional)
+    -H  Elasticsearch hostname:port (defaults to $eshost)
+    -n  On a host with multiple nodes, specify the node name to gather data for. Value should match node.name as defined in elasticsearch.yml
     -o  Script output directory (optional, defaults to support-diagnostics.[timestamp].[hostname])
     -nc Disable compression (optional)
 
+### Running on Windows
+
+PowerShell 4.0 and .NET Framework 4.5 are required in order to run the script on Windows, both which come built into Windows Server 2012 and Windows 8.1.  If you are running anything earlier than Windows Server 2012 or Windows 8.1, then you may need to install them manually.  See [How to Install Windows PowerShell 4.0](http://social.technet.microsoft.com/wiki/contents/articles/21016.how-to-install-windows-powershell-4-0.aspx) for more information.
+
+The script can be executed on Windows by calling either `support-diagnostics.bat` (preferred), or calling the `support-diagnostics.ps1` PowerShell script directly.  The purpose of the batch file is to enable the script to be run from a PowerShell console as well as the regular Windows command prompt (cmd.exe).  It also passes the necessary arguments to bypass the Windows PowerShell execution policy.
---
+++ bin/support-diagnostics.ps1
@@ -0,0 +1,181 @@
+<#
+.SYNOPSIS
+    Collects diagnostic information from your elasticsearch cluster.
+.DESCRIPTION
+    This script is used to gather diagnotistic information for elasticsearch support.  In order to gather the elasticsearch config and logs you must run this on a node within your elasticsearch cluster.
+.PARAMETER H
+    Elasticsearch hostname:port (defaults to $eshost)
+.PARAMETER n
+    On a host with multiple nodes, specify the node name to gather data for. Value should match node.name as defined in elasticsearch.yml
+.PARAMETER o
+    Script output directory (optional, defaults to ./support-diagnostics.[timestamp].[hostname])
+.PARAMETER nc
+    Disable compression (optional)
+#>
+
+Param(
+    [string]$H,
+    [string]$n,
+    [string]$o,
+    [switch]$nc
+)
+
+# Set defaults
+$esHost = 'http://localhost:9200/'
+$timestamp = Get-Date -format yyyyMMdd-HHmmss
+$hostName = [System.Net.Dns]::GetHostName()
+$outputDir = 'support-diagnostics'+'.'+$timestamp+'.'+$hostname
+$targetNode = '_local'
+
+If ($H) {
+    $esHost = 'http://'+$H+'/'
+}
+
+If ($n) {
+    $hostName = $n
+    $targetNode = $n
+}
+
+If ($o) {
+    $outputDir = $o
+}
+
+New-Item $outputDir -Type directory | Out-Null
+If (!(Test-Path $outputDir)) {
+    Write-Host Cannot write output file.
+    Exit
+}
+
+Write-Host 'Getting your configuration from the elasticsearch API'
+
+$connectionTest = Invoke-WebRequest $esHost
+If ($connectionTest.StatusCode -ne 200) {
+    Write-Host Error connecting to $esHost
+    Exit
+}
+ 
+$nodenameStatus = (Invoke-WebRequest ($esHost+'_nodes/'+$targetNode+'/settings?pretty')).RawContent | Select-String '"nodes" : { }'
+If ($nodenameStatus) {
+    Write-Host `n`nThe node/host name $hostName does not appear to be connected to your cluster.  This script will continue, however without gathering the log files or elasticsearch.yml`n`n
+}
+
+# Get the ES version
+$esVersion= (Invoke-RestMethod $esHost).version.number
+
+$nodes = (Invoke-RestMethod ($esHost+'_nodes/'+$targetNode+'/settings?pretty')).nodes
+$nodesPropertyName = $nodes.psobject.properties.name
+$nodeSettings = $nodes.$nodesPropertyName.settings
+
+If ($esVersion.StartsWith("0.9")) {
+    $esHomePath = $nodeSettings."path.home"
+    $esLogsPath = $nodeSettings."path.logs"
+} Else {
+    $esHomePath = $nodeSettings.path.home
+    $esLogsPath = $nodeSettings.path.logs
+}
+
+Write-Host 'Getting config'
+$esConfigPath = $esHomePath+'\config'
+# Check if the config directory we found exists
+If (!(Test-Path $esConfigPath)) {
+    # Sometimes the API returns a relative path.  If so, try prepending the home directory
+    $esConfigPath = $esHomePath+'\'+$esConfigPath
+    If (!(Test-Path $esConfigPath)) {
+        Write-Host `nCould not get your elasticsearch config.  Please add your elasticsearch config directory to the $outputDir'.zip' or to your ticket.`n`n
+        $esConfigPath = ''
+    }
+}
+
+If ($esConfigPath) {
+    cp -r $esConfigPath $outputDir\config
+}
+
+Write-Host 'Getting logs'
+If (!(Test-Path $esLogsPath)) {
+    $esLogsPath = $esHomePath+'\'+$esLogsPath
+    If (!(Test-Path $esLogsPath)) {
+        Write-Host `nCould not get your elasticsearch logs.  Please add your logs directory to the $outputDir+'.zip' or to your ticket.`n`n
+        $esLogsPath = ''
+    }
+}
+
+If ($esLogsPath) {
+    mkdir $outputDir\logs | Out-Null
+    cp $esLogsPath\*.log $outputDir\logs\
+}
+
+# API calls that work with all versions
+Write-Host 'Getting version'
+Invoke-WebRequest $esHost -OutFile $outputDir/version.json
+
+Write-Host "Getting _mapping"
+Invoke-WebRequest $esHost'/_mapping?pretty' -OutFile $outputDir/mapping.json
+
+Write-Host 'Getting _settings'
+Invoke-WebRequest $esHost'/_settings?pretty' -OutFile $outputDir/settings.json
+
+Write-Host 'Getting _cluster/settings'
+Invoke-WebRequest $esHost'/_cluster/settings?pretty' -OutFile $outputDir/cluster_settings.json
+
+Write-Host 'Getting _cluster/state'
+Invoke-WebRequest $esHost'/_cluster/state?pretty' -OutFile $outputDir/cluster_state.json
+
+Write-Host 'Getting _cluster/stats'
+Invoke-WebRequest $esHost'/_cluster/stats?pretty&human' -OutFile $outputDir/cluster_stats.json
+
+Write-Host 'Getting nodes info'
+Invoke-WebRequest $esHost'/_nodes/?all&pretty&human' -OutFile $outputDir/nodes.json
+
+Write-Host 'Getting _nodes/hot_threads'
+Invoke-WebRequest $esHost'/_nodes/hot_threads?threads=10' -OutFile $outputDir/nodes_hot_threads.txt
+
+# API calls that only work with 0.90
+If ($esVersion.StartsWith("0.9")) {
+    Write-Host 'Getting _nodes/stats'
+    Invoke-WebRequest $esHost'/_nodes/stats?all&pretty&human' -OutFile $outputDir/nodes_stats.json
+
+    Write-Host 'Getting indices stats'
+    Invoke-WebRequest $esHost'/_stats?all&pretty&human' -OutFile $outputDir/indices_stats.json
+} Else {
+    # API calls that only work with >1.0
+    Write-Host 'Getting _nodes/stats'
+    Invoke-WebRequest $esHost'/_nodes/stats?pretty&human' -OutFile $outputDir/nodes_stats.json
+
+    Write-Host 'Getting indices stats'
+    Invoke-WebRequest $esHost'/_stats?pretty&human' -OutFile $outputDir/indices_stats.json
+
+    Write-Host 'Getting _cat/allocation'
+    Invoke-WebRequest $esHost'/_cat/allocation?v' -OutFile $outputDir/allocation.txt
+
+    Write-Host 'Getting _cat/plugins'
+    Invoke-WebRequest $esHost'/_cat/plugins?v' -OutFile $outputDir/plugins.txt
+
+    Write-Host 'Getting _cat/recovery'
+    Invoke-WebRequest $esHost'/_cat/recovery?v' -OutFile $outputDir/cat_recovery.json
+}
+
+Write-Host 'Running netstat'
+netstat | Out-File $outputDir/netstat.txt
+
+Write-Host 'Running top'
+ps | sort -desc cpu | select -first 30 | Out-File $outputDir/top.txt
+
+Write-Host 'Running ps'
+Get-Process -Name *elasticsearch* | Out-File $outputDir/elasticsearch-process.txt
+
+Write-Host 'Output complete.  Creating zip.'
+Add-Type -Assembly System.IO.Compression.FileSystem
+$compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
+If ($nc) {
+    $compressionLevel = [System.IO.Compression.CompressionLevel]::NoCompression
+}
+$zipFile = $outputDir+'.zip'
+$source = $pwd.Path+'\'+$outputDir
+$destination = $pwd.Path+'\'+$zipFile
+If ($o) {
+    $source = $outputDir
+    $destination = $zipFile
+}
+[System.IO.Compression.ZipFile]::CreateFromDirectory($source, $destination, $compressionLevel, $false)
+
+Write-Host `nDone.  Created $zipFile`n`n
