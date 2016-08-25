0b04bf1af7a03e2709d2f6141f2700e1d2084644
Greg Nieman <greg.nieman@gmail.com>
2016-08-25 07:25:25 -0400

    Added command to check for zero based Compressed Oops

--- src/main/java/com/elastic/support/diagnostics/commands/CompressedPointersCmd.java
+++ src/main/java/com/elastic/support/diagnostics/commands/CompressedPointersCmd.java
@@ -3,74 +3,76 @@ package com.elastic.support.diagnostics.commands;
 import com.elastic.support.SystemProperties;
 import com.elastic.support.diagnostics.DiagnosticContext;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
-import java.util.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 public class CompressedPointersCmd extends AbstractDiagnosticCmd {
 
    public boolean execute(DiagnosticContext context) {
 
-      String os = checkOS();
-      Map<String, String> osCmds = (Map<String, String>) context.getConfig().get(os);
+      String pid = context.getPid();
 
       ProcessBuilder pb = new ProcessBuilder();
       pb.redirectErrorStream(true);
 
-      Iterator<Map.Entry<String, String>> iter = osCmds.entrySet().iterator();
       List cmds = new ArrayList();
 
-      while (iter.hasNext()) {
-         Map.Entry<String, String> entry =  iter.next();
-         String cmdLabel = entry.getKey();
-         String cmdText = entry.getValue();
+      try {
+
+         String maxHeap = getMaxHeapSize(pid, new File(context.getTempDir() + SystemProperties.fileSeparator + "jps.txt"));
+
+         String cmdText = "java -Xms1m ### -XX:+UnlockDiagnosticVMOptions -XX:+PrintCompressedOopsMode -version ";
+         cmdText = cmdText.replace("###", maxHeap);
+
+         StringTokenizer st = new StringTokenizer(cmdText, " ");
+         while (st.hasMoreTokens()) {
+            cmds.add(st.nextToken());
+         }
+
+         pb.redirectOutput(new File(context.getTempDir() + SystemProperties.fileSeparator + "compressed_ptrs.txt"));
+         pb.command(cmds);
+         Process pr = pb.start();
+         pr.waitFor();
+      } catch (Exception e) {
+         logger.error("Error processing system command:compressed_ptrs.");
          try {
-            // One off hack for process limits
-            if (cmdLabel.equals("proc-limit")) {
-               cmdText = "cat /proc/" + context.getPid() + "/limits";
-            }
+            FileOutputStream fos = new FileOutputStream(new File(context.getTempDir() + SystemProperties.fileSeparator + "compressed_ptrs.txt"), true);
+            PrintStream ps = new PrintStream(fos);
+            e.printStackTrace(ps);
+         } catch (Exception ie) {
+            logger.error("Error processing system command", ie);
+         }
+      } finally {
+         cmds.clear();
+      }
+      return true;
 
-            StringTokenizer st = new StringTokenizer(cmdText, " ");
-            while (st.hasMoreTokens()) {
-               cmds.add(st.nextToken());
-            }
+   }
 
-            pb.redirectOutput(new File(context.getTempDir() + SystemProperties.fileSeparator + cmdLabel + ".txt"));
-            pb.command(cmds);
-            Process pr = pb.start();
-            pr.waitFor();
-         } catch (Exception e) {
-            logger.error("Error processing system command:" + cmdLabel);
-            try{
-               FileOutputStream fos = new FileOutputStream(new File(context.getTempDir() + SystemProperties.fileSeparator + cmdLabel + ".txt"), true);
-               PrintStream ps = new PrintStream(fos);
-               e.printStackTrace(ps);
-            }
-            catch (Exception ie){
-               logger.error("Error processing system command", ie);
-            }
-         } finally {
-            cmds.clear();
+   private String getMaxHeapSize(String pid, File jpsOutput) throws Exception {
+
+      BufferedReader br = new BufferedReader(new FileReader(jpsOutput));;
+      String maxHeap = "";
+
+      String line = br.readLine();
+      while(line != null){
+
+         line = br.readLine();
+         if( (line.substring(0, 10)).contains(pid)) {
+            int start = line.indexOf("-Xmx");
+            int end = line.indexOf(" ", start);
+            maxHeap = line.substring(start, end);
+            break;
          }
       }
 
+      br.close();
 
-      return true;
-   }
+      return maxHeap;
 
-   public String checkOS() {
-      String osName = SystemProperties.osName.toLowerCase();
-      if (osName.contains("windows")) {
-         return "winOS";
-      } else if (osName.contains("linux")) {
-         return "linuxOS";
-      } else if (osName.contains("darwin") || osName.contains("mac os x")) {
-         return "macOS";
-      } else {
-         logger.error("Failed to detect operating system!");
-         throw new RuntimeException("Unsupported OS");
-      }
    }
 
 }
