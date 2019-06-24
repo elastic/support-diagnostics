ddb9d6976778e110d1ae1de521880a54b05e8979
Greg Nieman <nemonster@users.noreply.github.com>
2019-06-24 16:46:40 -0400

    Fix branch (#290)
    * Version change
    
    Change version
    Update Jackson dependency for sec. alert.
    
    * Fix issue with role check
    
    * Fix read of roles from json user object
    * Add bypass check for unsecured clusters.
    * Add unit test for role check logic.

--- src/main/java/com/elastic/support/diagnostics/DiagnosticService.java
+++ src/main/java/com/elastic/support/diagnostics/DiagnosticService.java
@@ -10,6 +10,7 @@ import com.elastic.support.rest.RestClientBuilder;
 import com.elastic.support.util.SystemProperties;
 import com.elastic.support.util.SystemUtils;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.File;
 import java.io.IOException;
@@ -57,14 +58,16 @@ public class DiagnosticService extends BaseService {
 
             dc.runDiagnostic(ctx, chain);
 
-            if(!ctx.isAuthorized()){
+            String user = ctx.getDiagnosticInputs().getUser();
+
+            if(StringUtils.isNotEmpty(user) && !ctx.isAuthorized()){
 
                 String border = SystemUtils.buildStringFromChar(60, '*');
                 logger.warn(SystemProperties.lineSeparator);
                 logger.warn(border);
                 logger.warn(border);
                 logger.warn(border);
-                logger.warn("The elasticsearch user entered: {} does not appear to have sufficient authorization to access all collected information", ctx.getDiagnosticInputs().getUser());
+                logger.warn("The elasticsearch user entered: {} does not appear to have sufficient authorization to access all collected information", user);
                 logger.warn("Some of the calls may not have completed successfully.");
                 logger.warn("If you are using a custom role please verify that it has the admin role for versions prior to 5.x or the superuser role for subsequent versions.");
                 logger.warn(border);
