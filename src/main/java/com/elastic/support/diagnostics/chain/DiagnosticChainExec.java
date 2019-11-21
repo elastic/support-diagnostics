a2f3aa39b4b234bf4d9facf0838780d9ac47eae7
Greg Nieman <greg.nieman@elastic.co>
2019-11-21 15:18:38 -0500

    Refactored command chain
    Moved pathways out of configuration into code

--- src/main/java/com/elastic/support/diagnostics/chain/DiagnosticChainExec.java
+++ src/main/java/com/elastic/support/diagnostics/chain/DiagnosticChainExec.java
@@ -2,22 +2,42 @@ package com.elastic.support.diagnostics.chain;
 
 import com.elastic.support.config.Constants;
 import com.elastic.support.diagnostics.DiagnosticException;
+import com.elastic.support.diagnostics.commands.*;
 import com.elastic.support.util.SystemProperties;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.List;
-import java.util.Map;
 
 public class DiagnosticChainExec {
 
     private static Logger logger = LogManager.getLogger(DiagnosticChainExec.class);
 
-    public void runDiagnostic(DiagnosticContext context, List<String> chain) {
+    public static void runDiagnostic(DiagnosticContext context, String type) {
 
         try {
-            Chain diagnostic = new Chain(chain);
-            diagnostic.execute(context);
+            if("standard".equals(type) || "remote".equals(type)) {
+                new GenerateManifestCmd().execute(context);
+                new DiagVersionCheckCmd().execute(context);
+                new VersionCheckCmd().execute(context);
+                new UserRoleCheckCmd().execute(context);
+                new RunClusterQueriesCmd().execute(context);
+            }
+
+            if("standard".equals(type)){
+                new HostIdentifierCmd().execute(context);
+                new CollectLogsCmd().execute(context);
+                new SystemCallsCmd().execute(context);
+                new SystemDigestCmd().execute(context);
+            }
+
+            if("logstash".equals(type)){
+                new GenerateManifestCmd().execute(context);
+                new DiagVersionCheckCmd().execute(context);
+                new RunLogstashQueriesCmd().execute(context);
+                new SystemCallsCmd().execute(context);
+                new SystemDigestCmd().execute(context);
+            }
+
 
         } catch (DiagnosticException de) {
             throw de;
