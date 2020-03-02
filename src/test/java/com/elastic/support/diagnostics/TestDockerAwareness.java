c861fc03e4af626cf15d036df604dcc945f413b6
Greg Nieman <greg.nieman@elastic.co>
2020-03-02 11:22:27 -0500

    Update Docs plus minor tweaks
    1. Updated documentation
    2. Cleaned out tests used for local debugging.
    3. Fixed test for Docker check that was inputting wrong file.
    4. Fixed input description for hostname verification.

--- src/test/java/com/elastic/support/diagnostics/TestDockerAwareness.java
+++ src/test/java/com/elastic/support/diagnostics/TestDockerAwareness.java
@@ -8,8 +8,7 @@ import org.junit.jupiter.api.Test;
 import java.io.InputStream;
 import java.util.List;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class TestDockerAwareness {
 
@@ -21,9 +20,9 @@ public class TestDockerAwareness {
             List<String> entries = IOUtils.readLines(in, Constants.UTF_8);
             assertTrue(SystemUtils.checkCGroupEntries(entries));
 
-            in = this.getClass().getClassLoader().getResourceAsStream("proc/1/cgroup-yes");
+            in = this.getClass().getClassLoader().getResourceAsStream("proc/1/cgroup-no");
             entries = IOUtils.readLines(in, Constants.UTF_8);
-            assertFalse(SystemUtils.checkCGroupEntries(entries));
+            assertEquals(false, SystemUtils.checkCGroupEntries(entries));
 
         } catch (Exception e) {
             e.printStackTrace();
