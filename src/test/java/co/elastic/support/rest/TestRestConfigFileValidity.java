19390da92e3bab135bc015b6bbd797f474bba279
Antonio Bellezza <antonio.bellezza@elastic.co>
2021-12-06 21:39:00 +0100

    Add leading / to elastic rest endpoints which were missing it.

--- src/test/java/co/elastic/support/rest/TestRestConfigFileValidity.java
+++ src/test/java/co/elastic/support/rest/TestRestConfigFileValidity.java
@@ -13,6 +13,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.Map;
 
 import static org.junit.Assert.*;
@@ -21,20 +22,20 @@ public class TestRestConfigFileValidity {
 
     private static final Logger logger = LogManager.getLogger(TestRestConfigFileValidity.class);
 
-    protected static Semver sem= new Semver("9.9.999", Semver.SemverType.NPM);
+    protected static Semver sem = new Semver("9.9.999", Semver.SemverType.NPM);
 
     @Test
     public void validateElasticConfigVersioning() throws DiagnosticException {
-        // validates whether each set of version entries has exactly one valid outcome.
-        Map<String, Object> restEntriesConfig = JsonYamlUtils.readYamlFromClasspath("elastic-rest.yml", true);
-        validateEntries(restEntriesConfig);
-        restEntriesConfig = JsonYamlUtils.readYamlFromClasspath("logstash-rest.yml", true);
-        validateEntries(restEntriesConfig);
+        // validates each set of version entries.
+        for (String yamlfile : Arrays.asList("elastic-rest.yml", "logstash-rest.yml", "kibana-rest.yml", "monitoring-rest.yml")) {
+            Map<String, Object> restEntriesConfig = JsonYamlUtils.readYamlFromClasspath(yamlfile, true);
+            validateEntries(restEntriesConfig);
+        }
     }
 
 
     private void validateEntries(Map<String, Object> config) {
-        for( Map.Entry<String, Object>entry : config.entrySet() ) {
+        for (Map.Entry<String, Object> entry : config.entrySet()) {
 
             Map<String, Object> values = (Map) entry.getValue();
 
@@ -42,14 +43,16 @@ public class TestRestConfigFileValidity {
 
             int nbrValid = 0;
 
+            // Urls should have a leading /
             // For each entry there should be only 1 valid url.
             for (Map.Entry<String, String> url : urls.entrySet()) {
+                assertTrue(url.getValue(), url.getValue().startsWith("/"));
                 if (sem.satisfies(url.getKey())) {
                     nbrValid++;
                 }
             }
 
-            assertTrue( nbrValid == 1 );
+            assertTrue(nbrValid == 1);
 
         }
 
