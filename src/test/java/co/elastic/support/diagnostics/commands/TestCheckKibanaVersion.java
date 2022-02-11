4ec74d4a6362b6ce321433dd7b806b7f4182856c
Antonio Bellezza <antbell@users.noreply.github.com>
2022-02-11 18:09:30 +0100

    Read Kibana version from /api/stats instead of /api/settings (#572)
    * Try /api/stats first to get Kibana version
    
    * Add test for computation of kibana version from /api/stats
    
    * Only use /api/stats endpoint for Kibana version detection

--- src/test/java/co/elastic/support/diagnostics/commands/TestCheckKibanaVersion.java
+++ src/test/java/co/elastic/support/diagnostics/commands/TestCheckKibanaVersion.java
@@ -66,7 +66,7 @@ public class TestCheckKibanaVersion {
         mockServer.reset();
     }
 
-    private void initializeKibana() {
+    private void initializeKibanaSettings(String version) {
 
         mockServer
                 .when(
@@ -76,7 +76,24 @@ public class TestCheckKibanaVersion {
                 )
                 .respond(
                         response()
-                                .withBody("{\"cluster_uuid\":\"RLtzkhfBRUadN4WZ8fnnog\",\"settings\":{\"xpack\":{\"default_admin_email\":null},\"kibana\":{\"uuid\":\"a4f369ef-fecd-46b7-8b16-c6c3f885d9ec\",\"name\":\"13d5e793ea51\",\"index\":\".kibana\",\"host\":\"0.0.0.0\",\"port\":18648,\"locale\":\"en\",\"transport_address\":\"0.0.0.0:18648\",\"version\":\"6.5.0\",\"snapshot\":false,\"status\":\"green\"}}}")
+                                .withBody("{\"cluster_uuid\":\"RLtzkhfBRUadN4WZ8fnnog\",\"settings\":{\"xpack\":{\"default_admin_email\":null},\"kibana\":{\"uuid\":\"a4f369ef-fecd-46b7-8b16-c6c3f885d9ec\",\"name\":\"13d5e793ea51\",\"index\":\".kibana\",\"host\":\"0.0.0.0\",\"port\":18648,\"locale\":\"en\",\"transport_address\":\"0.0.0.0:18648\",\"version\":\"" + version + "\",\"snapshot\":false,\"status\":\"green\"}}}")
+                                .withStatusCode(200)
+                );
+
+
+    }
+
+    private void initializeKibanaStats(String version) {
+
+        mockServer
+                .when(
+                        request()
+                                .withMethod("GET")
+                                .withPath("/api/stats")
+                )
+                .respond(
+                        response()
+                                .withBody("{\"kibana\":{\"uuid\":\"669ae985-31f7-493b-9910-522cac4d5479\",\"name\":\"6f5485cce678\",\"index\":\".kibana\",\"host\":\"0.0.0.0\",\"locale\":\"en\",\"transport_address\":\"0.0.0.0:18117\",\"version\":\"" + version + "\",\"snapshot\":false,\"status\":\"green\"}}")
                                 .withStatusCode(200)
                 );
 
@@ -84,36 +101,47 @@ public class TestCheckKibanaVersion {
     }
 
     @Test
-    public void testQueriesForKibana() throws DiagnosticException {
-		initializeKibana();
+    public void testQueriesForKibanaWhenStats() throws DiagnosticException {
+        initializeKibanaStats("8.1.2");
         Semver version = new CheckKibanaVersion().getKibanaVersion(httpRestClient);
-        // the version 6.5.0 was defined on the json object on the mockServer boby.
-   		assertEquals("6.5.0", version.getValue());
+        assertEquals("8.1.2", version.getValue());
     }
 
+    @Test
+    public void testQueriesForKibanaWhenStatsWithRC() throws DiagnosticException {
+        initializeKibanaStats("8.0.0-rc2");
+        Semver version = new CheckKibanaVersion().getKibanaVersion(httpRestClient);
+        assertEquals("8.0.0-rc2", version.getValue());
+    }
+
+    @Test
+    public void testQueriesForKibanaWhenStatsAndSettings() throws DiagnosticException {
+        initializeKibanaStats("6.5.0");
+        initializeKibanaSettings("6.5.0");
+        Semver version = new CheckKibanaVersion().getKibanaVersion(httpRestClient);
+        assertEquals("6.5.0", version.getValue());
+    }
 
     /**
      * version is Mandatory as we use the version to define the APIs that will be executed
      */
     @Test
     public void testQueriesForKibanaEmptyVersion() {
-        // the body is different here than in the previous test
-        // HERE we have removed the version from the JSON response to trigger the expection
+        // The response body contains no version
         mockServer
                 .when(
                         request()
                                 .withMethod("GET")
-                                .withPath("/api/settings")
+                                .withPath("/api/stats")
                 )
                 .respond(
                         response()
-                                .withBody("{\"cluster_uuid\":\"RLtzkhfBRUadN4WZ8fnnog\",\"settings\":{\"xpack\":{\"default_admin_email\":null},\"kibana\":{\"uuid\":\"a4f369ef-fecd-46b7-8b16-c6c3f885d9ec\",\"name\":\"13d5e793ea51\",\"index\":\".kibana\",\"host\":\"0.0.0.0\",\"port\":18648,\"locale\":\"en\",\"transport_address\":\"0.0.0.0:18648\",\"snapshot\":false,\"status\":\"green\"}}}")
+                                .withBody("{\"kibana\": {}}")
                                 .withStatusCode(200)
                 );
 
         try {
             Semver version = new CheckKibanaVersion().getKibanaVersion(httpRestClient);
-            // if they are more than one node in Kibana we need to throw an Exception
             assertTrue(false);
         } catch (DiagnosticException e) {
             assertEquals(e.getMessage(), "Kibana version format is wrong - unable to continue. ()");
@@ -126,20 +154,7 @@ public class TestCheckKibanaVersion {
      */
     @Test
     public void testQueriesForKibanaCorruptedVersion() {
-        // the body is different here than in the previous test
-        // HERE we have removed the version from the JSON response to trigger the expection
-        mockServer
-                .when(
-                        request()
-                                .withMethod("GET")
-                                .withPath("/api/settings")
-                )
-                .respond(
-                        response()
-                                .withBody("{\"cluster_uuid\":\"RLtzkhfBRUadN4WZ8fnnog\",\"settings\":{\"xpack\":{\"default_admin_email\":null},\"kibana\":{\"uuid\":\"a4f369ef-fecd-46b7-8b16-c6c3f885d9ec\",\"name\":\"13d5e793ea51\",\"index\":\".kibana\",\"host\":\"0.0.0.0\",\"port\":18648,\"locale\":\"en\",\"transport_address\":\"0.0.0.0:18648\",\"version\":\"a.v.c\",\"snapshot\":false,\"status\":\"green\"}}}")
-                                .withStatusCode(200)
-                );
-
+        initializeKibanaStats("a.v.c");
         try {
             Semver version = new CheckKibanaVersion().getKibanaVersion(httpRestClient);
             // if they are more than one node in Kibana we need to throw an Exception
@@ -155,13 +170,12 @@ public class TestCheckKibanaVersion {
      */
     @Test
     public void testQueriesForKibanaTextWithVersion() {
-        // the body is different here than in the previous test
-        // HERE we have removed the version from the JSON response to trigger the expection
+        initializeKibanaStats("test-6.5.1");
         mockServer
                 .when(
                         request()
                                 .withMethod("GET")
-                                .withPath("/api/settings")
+                                .withPath("/api/stats")
                 )
                 .respond(
                         response()
