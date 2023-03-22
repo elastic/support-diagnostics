a53ca0363db849aa3fa8eeee4ba18c40036de5c9
Youhei Sakurai <sakurai.youhei@gmail.com>
2023-03-23 07:17:51 +0900

    Revive --indexName option as --targetsuffix (#613)
    Closes #537

--- src/main/java/co/elastic/support/monitoring/MonitoringImportInputs.java
+++ src/main/java/co/elastic/support/monitoring/MonitoringImportInputs.java
@@ -15,6 +15,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.beryx.textio.StringInputReader;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.List;
 
@@ -26,6 +29,9 @@ public class MonitoringImportInputs extends ElasticRestClientInputs {
 
     @Parameter(names = {"--clustername"}, description = "Overrides the name of the imported cluster.")
     protected String clusterName;
+
+    @Parameter(names = {"--targetsuffix"}, description = "Overrides the suffix of the import target from the date, appending it to e.g. .monitoring-es-7-diag-import-.")
+    protected String targetSuffix;
 
     @Parameter(names = {"-i", "--input"}, description = "Required: The archive that you wish to import into Elastic Monitoring. This must be in the format produced by the diagnostic export utility.")
     protected String input;
@@ -48,6 +54,11 @@ public class MonitoringImportInputs extends ElasticRestClientInputs {
                 .withMinLength(0)
                 .read("Specify an alternate name for the imported cluster or hit enter to use original cluster name:");
 
+        targetSuffix = textIOManager.textIO.newStringInputReader()
+                .withInputTrimming(true)
+                .withDefaultValue((DateTimeFormatter.ofPattern("yyyy-MM-dd").format(ZonedDateTime.now(ZoneId.of("+0")))))
+                .read("Specify an alternate suffix for the import target or hit enter for the default generated name:");
+
         input = textIOManager.textIO.newStringInputReader()
                 .withInputTrimming(true)
                 .withValueChecker((String val, String propname) -> validateRequiredFile(val))
@@ -63,6 +74,7 @@ public class MonitoringImportInputs extends ElasticRestClientInputs {
         List<String> errors = super.parseInputs(textIOManager, args);
 
         errors.addAll(ObjectUtils.defaultIfNull(validateId(clusterName), emptyList));
+        errors.addAll(ObjectUtils.defaultIfNull(validateId(targetSuffix), emptyList));
         errors.addAll(ObjectUtils.defaultIfNull(validateRequiredFile(input), emptyList));
 
         return errors;
@@ -82,6 +94,7 @@ public class MonitoringImportInputs extends ElasticRestClientInputs {
     public String toString() {
         return "MonitoringImportInputs{" +
                 "clusterName='" + clusterName + '\'' +
+                ", targetSuffix='" + targetSuffix + '\'' +
                 ", input='" + input + '\'' +
                 ", proxyHostReader=" + proxyHostReader +
                 '}';
