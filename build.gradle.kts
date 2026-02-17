b0282f671214e6b4c5e0b6d08a75ff81569d49fb
Chris Earle <pickypg@gmail.com>
2026-02-17 16:35:34 -0700

    [Tests] Remove Defunct mockserver Dependency
    This replaces it with wiremock.

--- build.gradle.kts
+++ build.gradle.kts
@@ -79,7 +79,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-engine:6.0.3")
     testImplementation("org.junit.platform:junit-platform-launcher:1.14.1")
     testRuntimeOnly("org.junit.platform:junit-platform-surefire-provider:1.3.2")
-    testImplementation("org.mock-server:mockserver-netty:5.15.0")
+    testImplementation("org.wiremock:wiremock:3.13.2")
 }
 
 // ---------------------------------------------------------------------------
