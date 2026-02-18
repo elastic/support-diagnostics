823f4789de47a1f19f4a6a6a9e2e61e8bf2c326c
Chris Earle <pickypg@users.noreply.github.com>
2026-02-17 17:35:48 -0700

    [Build] Add Ability to update NOTICE.txt file (#962)
    This adds the same functionality from the Maven build and drops test dependencies since those are not part of the distribution.

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    mavenCentral()
+    gradlePluginPortal()
+}
+
+dependencies {
+    implementation("com.github.jk1:gradle-license-report:3.1.1")
+}
