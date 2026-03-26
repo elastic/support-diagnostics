6e826184b2290fb26c5a32cf101aeeb3e45333d4
andsel <selva.andre@gmail.com>
2026-03-26 11:37:21 +0100

    Removed runApplication utility task for dev

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,15 +5,11 @@ plugins {
     `maven-publish`
     signing
     id("com.github.jk1.dependency-license-report")
-//    application
 }
 
 group = property("group") as String
 version = property("version") as String
 
-//application {
-//    mainClass.set("co.elastic.support.diagnostics.DiagnosticApp")
-//}
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
@@ -175,20 +171,6 @@ val distZipChecksum by tasks.registering {
 // Wire distribution tasks into the build lifecycle
 tasks.named("build") {
     dependsOn(distZip, distZipChecksum, copyDependencies)
-}
-
-tasks.register<JavaExec>("runApplication") {
-    group = "Execution"
-    description = "Runs the specified main class"
-
-    // Set the full path to your main class
-    mainClass.set("co.elastic.support.diagnostics.DiagnosticApp")
-
-    // Use the compiled classes and dependencies as the classpath
-    classpath = sourceSets["main"].runtimeClasspath
-
-    // Optional: Pass command-line arguments
-    // args("arg1", "arg2")
 }
 
 // ---------------------------------------------------------------------------
